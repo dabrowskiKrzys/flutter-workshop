1d89f53833665ec509fb487253acf769fca286de
dabrowskiKrzys <krzysztof.k.dabrowski@gmail.com>
2019-02-19 22:10:10 +0100

    work on excersises

--- answers/1_hello_world/lib/main.dart
+++ answers/1_hello_world/lib/main.dart
@@ -10,12 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My first app."),
-        ),
-        body: HomeScreen(),
-      ),
+      home: HomeScreen(),
     );
   }
 }
--- answers/1_hello_world/lib/screens/home_screen.dart
+++ answers/1_hello_world/lib/screens/home_screen.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Hello World!",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My first app."),
+      ),
+      body: Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
