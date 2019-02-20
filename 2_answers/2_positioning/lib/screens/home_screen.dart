751e7d4a5257139598061bef57eea9588ff9dab1
dabrowskiKrzys <krzysztof.k.dabrowski@gmail.com>
2019-02-20 19:29:52 +0100

    positioning answerr 2

--- 2_answers/2_positioning/lib/screens/home_screen.dart
+++ 2_answers/2_positioning/lib/screens/home_screen.dart
@@ -7,15 +7,50 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("My first app."),
       ),
-      body: Center(
-        child: Text(
-          "Hello World!",
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _buildHeader(),
+            _buildRow("Alarm", Icons.alarm),
+            Divider(),
+            _buildRow("Airport shuttle", Icons.airport_shuttle),
+            Divider(height: 5.0 ,),
+            _buildRow("Casino", Icons.casino),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Text _buildHeader() {
+    return Text(
+      "Hello world",
+      style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.blueAccent),
+    );
+  }
+
+  Row _buildRow(String text, IconData iconData) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          text,
           style: TextStyle(
-            fontSize: 20.0,
+            fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
-      ),
+        Icon(
+          iconData,
+          size: 40.0,
+          color: Colors.redAccent,
+        ),
+      ],
     );
   }
 }
