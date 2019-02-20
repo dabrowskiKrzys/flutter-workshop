24904d2ae595219d027ea6eb15e7f0f2245670a4
dabrowskiKrzys <krzysztof.k.dabrowski@gmail.com>
2019-02-20 21:19:53 +0100

    stateful

---
+++ 2_answers/4_stateful/lib/screens/alarm_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class AlarmScreen extends StatefulWidget {
+  final String _alarmText;
+
+  AlarmScreen(this._alarmText);
+
+  @override
+  AlarmScreenState createState() => AlarmScreenState();
+}
+
+class AlarmScreenState extends State<AlarmScreen> {
+  Color color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget._alarmText),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("click");
+          var random = Random();
+          setState(() {
+            color = Color.fromRGBO(
+              random.nextInt(255),
+              random.nextInt(255),
+              random.nextInt(255),
+              1.0,
+            );
+          });
+        },
+        child: Icon(Icons.casino),
+      ),
+      body: Center(
+        child: Icon(
+          Icons.alarm,
+          size: 80.0,
+          color: color,
+        ),
+      ),
+    );
+  }
+}
